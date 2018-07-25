f769c098a0ecca7ec8460b114ba39df28251d8d7
Harsh Pathania <akshatrana2007@gmail.com>
2018-07-25 00:19:41 -0500

    Add message to browser UI

--- public/js/app.js
+++ public/js/app.js
@@ -7,6 +7,8 @@ socket.on('connect',function () {
 socket.on('message', function (message) {
     console.log('New message:');
     console.log(message.text);
+    
+    jQuery('.messages').append('<p>'+ message.text +'</p>');
 });
 
 // Handles submitting of new message
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ io.on('connection',function(socket){
     socket.on('message', function(message){
         console.log('Message received: ' + message.txt);
         
-        socket.broadcast.emit('message',message);
+        io.emit('message',message);
     });
     socket.emit('message',{
         text: 'Welcome to the chat application!'
