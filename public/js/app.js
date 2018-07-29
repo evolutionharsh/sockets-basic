142c09c407fcbd9b396c3adfcc41c577f4715dd7
Harsh Pathania <akshatrana2007@gmail.com>
2018-07-28 21:12:29 -0500

    show timestamp on UI

--- public/js/app.js
+++ public/js/app.js
@@ -5,10 +5,11 @@ socket.on('connect',function () {
 });
 
 socket.on('message', function (message) {
+    var momentTimestamp = moment.utc(message.timestamp);
     console.log('New message:');
     console.log(message.text);
     
-    jQuery('.messages').append('<p>'+ message.text +'</p>');
+    jQuery('.messages').append('<p><strong>'+momentTimestamp.local().format('h:mm a')+': </strong>'+ message.text +'</p>');
 });
 
 // Handles submitting of new message
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var PORT = process.env.PORT || 3000;
+var moment = require('moment');
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
@@ -9,11 +10,12 @@ io.on('connection',function(socket){
     console.log('User connected via socket.io!');
     socket.on('message', function(message){
         console.log('Message received: ' + message.txt);
-        
+        message.timestamp = moment().valueOf();
         io.emit('message',message);
     });
     socket.emit('message',{
-        text: 'Welcome to the chat application!'
+        text: 'Welcome to the chat application!',
+        timestamp: moment().valueOf()
     });
 });
 
